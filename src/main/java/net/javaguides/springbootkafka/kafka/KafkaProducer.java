9cd7b0fc8eeada64d5f1a6907f1534f73cc20ef2
daruan <daruan.rst@gmail.com>
2022-06-04 11:06:22 -0300

    Creating a Kafka Producer

---
+++ src/main/java/net/javaguides/springbootkafka/kafka/KafkaProducer.java
@@ -0,0 +1,22 @@
+package net.javaguides.springbootkafka.kafka;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class KafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    //      KafkaTemplate<Key   , Value >
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        LOGGER.info(String.format("Message sent %s" , message));
+        kafkaTemplate.send("javaguide", message);
+    }
+}
